a1d156aea6eacf80ce84dd98dbe55f0c29e18bb8
Roeung Bunchhorng <roeungbunchhorng37@gmail.com>
2026-09-21 12:15:24 +0700

    create product

--- app.py
+++ app.py
@@ -1,7 +1,11 @@
 from flask import Flask, render_template, request, redirect, url_for
 from  connection import conn
+from werkzeug.utils import secure_filename
+import os
 
 app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = 'static/uploads/products'
+os.makedirs(app.config['UPLOAD_FOLDER'], exist_ok=True)
 
 @app.route('/')
 def dashboard():
@@ -64,10 +68,37 @@ def delete_category(id):
 
 @app.route('/product')
 def index_product():
-    return render_template('products/index.html')
+    cursor = conn.cursor()
+    cursor.execute('SELECT * FROM products')
+    products = cursor.fetchall()
+    return render_template('products/index.html', products=products)
 
 @app.route('/product/create', methods=['GET', 'POST'])
 def create_product():
+    if request.method == "POST":
+        name = request.form['name']
+        price = request.form['price']
+        stock = request.form['stock']
+        
+        image_file = request.files.get('image')
+        image_name = None
+
+        if image_file and image_file.filename != '':
+            image_name = secure_filename(image_file.filename)
+            image_path = os.path.join(app.config['UPLOAD_FOLDER'], image_name)
+        
+            image_file.save(image_path)
+        cursor = conn.cursor()
+        sql = """
+            INSERT INTO products(name, price, stock, image)
+            VALUES(%s, %s, %s, %s)
+        """
+        cursor.execute(sql, (name, price, stock, image_name))
+        conn.commit()
+        cursor.close()
+        
+        return redirect(url_for('index_product'))
+        
     return render_template('products/create.html')
 
 if __name__=="__main__":
